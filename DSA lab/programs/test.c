4266634060cedc8108dbb9b070c44d96e6269be3
subhendu17620 <subhendu17620@gmail.com>
2019-12-15 22:54:50 +0530

    update

--- DSA lab/programs/test.c
+++ DSA lab/programs/test.c
@@ -1,138 +1,35 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-struct node
-{
-    int data;
-    struct node *next;
+struct student{
+  int rl;
+  char name[10];
+  char address[20];
+  int age;
+  float marks;
 };
 
-struct node *top = NULL;
+struct student s[20];
 
-void push(int x)
-{
-    struct node *newnode;
-    newnode = (struct node *)malloc(sizeof(struct node));
-    newnode->data = x;
-    newnode->next = NULL;
-    if (top == NULL)
-    {
-        top = newnode;
-        top->next = NULL;
-    }
-    else
-    {
-        newnode->next = top;
-        top = newnode;
-    }
+void get_data(int n);
+void print_data(int n);
+
+void main() {
+  printf("Enter number of students: ");
+  int count;
+  scanf("%d",&count);
+  get_data(count);
+  print_data(count);
 }
-void pushbfr(int n, int x)
-{
-
-    struct node *newnode;
-    newnode = (struct node *)malloc(sizeof(struct node));
-    newnode->data = x;
-
-    struct node *temp = top, *back;
-
-    while (temp->data != n)
-    {
-        back = temp;
-        temp = temp->next;
-    }
-    back->next = newnode;
-    newnode->next = temp;
+void get_data(int n){
+  for (int i = 0; i < n; i++) {
+    printf("Enter student %d data :",i+1 );
+    scanf("%d %s %s %d %f",&s[i].rl,&s[i].name,&s[i].address,&s[i].age,&s[i].marks);
+  }
 }
+void print_data(int n){
+  for (int i = 0; i < n; i++) {
+    printf("\nDetails of student %d",i+1);
+    printf("\nRoll no: %d name: %s address: %s age: %d marks: %f",s[i].rl,s[i].name,s[i].address,s[i].age,s[i].marks);
+  }
 
-void pushaftr(int n, int x)
-{
-
-    struct node *newnode;
-    newnode = (struct node *)malloc(sizeof(struct node));
-    newnode->data = x;
-
-    struct node *temp = top, *aftr;
-    while (temp->data!=n)
-    {
-        aftr=temp;
-        temp=temp->next;
-    }
-    
-    temp->next=newnode;
-    newnode->next=aftr;
-
-   
-}
-
-void pop()
-{
-    struct node *temp = top;
-    if (top == NULL)
-    {
-        printf("Empty stack ");
-    }
-    else
-    {
-        top = top->next;
-        free(temp);
-    }
-}
-void print()
-{
-    struct node *temp = top;
-    if (top == NULL)
-        printf("Empty Stakc");
-    else
-    {
-        while (temp != NULL)
-        {
-            printf("%d-", temp->data);
-            temp = temp->next;
-        }
-    }
-}
-int main(int argc, char const *argv[])
-{
-    // int ITEM,prev, choice;
-    push(2);
-    push(3);
-    push(4);
-    push(5);
-    push(6);
-
-    // print();
-    // printf("huhi");
-    // pushbfr(5, 8);
-    // print();
-    pushaftr(4,10);
-    print();
-    // while (1)
-    // {
-    //     printf("Enter Choice (1: PRINT, 2: PUSH, 3: POP, 4: Exit..:");
-    //     scanf("%d", &choice);
-
-    //     switch (choice)
-    //     {
-    //     case 1:
-    //         print();
-    //         break;
-    //     case 2:
-    //         printf("Enter Item to be insert :");
-    //         scanf("%d", &ITEM);
-    //         printf("prev one: ");
-    //         scanf("%d",&prev);
-    //         push(ITEM);
-    //         pushbfr(prev,ITEM);
-    //         break;
-    //     case 3:
-    //         pop();
-    //         break;
-    //     case 4:
-    //         exit(0);
-    //     default:
-    //         printf("\nInvalid choice.");
-    //         break;
-    //     }
-    // }
-    return 0;
 }
