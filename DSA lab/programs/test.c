96c551b0cf43bde77e3e6ada6644f92aeffc9203
subhendu17620 <subhendu17620@gmail.com>
2019-12-21 16:46:47 +0530

    update

--- DSA lab/programs/test.c
+++ DSA lab/programs/test.c
@@ -1,35 +1,7 @@
 #include <stdio.h>
 
-struct student{
-  int rl;
-  char name[10];
-  char address[20];
-  int age;
-  float marks;
-};
-
-struct student s[20];
-
-void get_data(int n);
-void print_data(int n);
-
 void main() {
-  printf("Enter number of students: ");
-  int count;
-  scanf("%d",&count);
-  get_data(count);
-  print_data(count);
-}
-void get_data(int n){
-  for (int i = 0; i < n; i++) {
-    printf("Enter student %d data :",i+1 );
-    scanf("%d %s %s %d %f",&s[i].rl,&s[i].name,&s[i].address,&s[i].age,&s[i].marks);
-  }
-}
-void print_data(int n){
-  for (int i = 0; i < n; i++) {
-    printf("\nDetails of student %d",i+1);
-    printf("\nRoll no: %d name: %s address: %s age: %d marks: %f",s[i].rl,s[i].name,s[i].address,s[i].age,s[i].marks);
-  }
-
+  int ar[]={1,2,3,4,5,4,4,3};
+  printf("%d",sizeof(ar));
+  
 }
